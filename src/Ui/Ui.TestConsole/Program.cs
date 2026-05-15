a6a2ae308c9c2a8fbcd6a466a0d16b9dfd03452b
codingfreak <sprinter@codingfreaks.de>
2026-05-15 20:22:41 +0200

    Created poc console with base.props

---
+++ src/Ui/Ui.TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace codingfreaks.obscene.Ui.TestConsole
+{
+    internal class Program
+    {
+        #region methods
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        #endregion
+    }
+}
