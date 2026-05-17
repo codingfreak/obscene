66a793f0052c80e19560ac9901bdbf32023a95f5
codingfreak <sprinter@codingfreaks.de>
2026-05-17 15:27:44 +0200

    Added abstractions to solution

---
+++ src/Logic/Logic.Abstracts/Interfaces/IGeometry.cs
@@ -0,0 +1,24 @@
+﻿namespace codingfreaks.obscene.Logic.Abstracts.Interfaces
+{
+    using System.Drawing;
+
+    /// <summary>
+    /// Must be implemented by all types which represent forms drawn to the screen.
+    /// </summary>
+    public interface IGeometry
+    {
+        #region methods
+
+        /// <summary>
+        /// Draws the form to the screen or refreshes it.
+        /// </summary>
+        void Draw();
+
+        /// <summary>
+        /// Gets or sets the absolute position on the screen.
+        /// </summary>
+        Point Position { get; set; }
+
+        #endregion
+    }
+}
